ff54513e537d46e98efa4636321970d605ab6488
onbgh823 <68522421+onbgh823@users.noreply.github.com>
2020-08-20 12:44:07 +0900

    제이커리 슬라이드업 다운

---
+++ js/header.js
@@ -0,0 +1,14 @@
+(function($){
+
+    $('.depth1 > li').hover(
+        function(){
+            $(this).find('.depth2').stop().slideDown(700)
+        },
+        function(){
+            $(this).find('.depth2').stop().slideUp(500)
+        }
+    )
+
+
+
+})(jQuery)
