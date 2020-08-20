2dcd23d29e22f13814ce80eb19d06ccb880ff9dc
onbgh823 <68522421+onbgh823@users.noreply.github.com>
2020-08-20 18:08:47 +0900

    each

--- js/header.js
+++ js/header.js
@@ -1,14 +1,27 @@
 (function($){
 
-    $('.depth1 > li').hover(
-        function(){
-            $(this).find('.depth2').stop().slideDown(700)
-        },
-        function(){
-            $(this).find('.depth2').stop().slideUp(500)
-        }
-    )
+    // $('.depth1 > li').hover(
+    //     function(){
+    //         $(this).find('.depth2').stop().slideDown(700)
+    //     },
+    //     function(){
+    //         $(this).find('.depth2').stop().slideUp(500)
+    //     }
+    // )
 
+    //each method
+    $('.depth1 > li').each(function(){
+        $(this).hover(
+            function(){
+                $(this).find('.depth2')
+                .stop().slideDown(700)
+            },
+            function(){
+                $(this).find('.depth2')
+                .stop().slideUp(500)
+            }
+        )
+    })
 
 
 })(jQuery)
